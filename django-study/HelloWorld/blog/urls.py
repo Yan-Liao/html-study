cc61598f89a0de576c3b4a214f2a5a1ae8a4eb6f
YanLiao <571764472@qq.com>
2020-05-31 19:57:38 +0800

    5/31

--- django-study/HelloWorld/blog/urls.py
+++ django-study/HelloWorld/blog/urls.py
@@ -19,5 +19,8 @@ from . import views
 
 
 urlpatterns = [
-    path('index/', views.index),
+    path('index/', views.index, name='index'),
+    path('article/<int:article_id>/', views.article_page, name='article_page'),
+    path('edit/<int:article_id>/', views.edit_page, name='edit_page'),
+    path('edit/action', views.edit_action, name='edit_action'),
 ]

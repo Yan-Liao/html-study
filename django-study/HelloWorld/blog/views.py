6970acd8e88687fab8282d15b6030291fafacbc4
YanLiao <571764472@qq.com>
2020-05-31 10:03:52 +0800

    5/31

--- django-study/HelloWorld/blog/views.py
+++ django-study/HelloWorld/blog/views.py
@@ -4,5 +4,5 @@ from . import models
 
 
 def index(request):
-    article = models.Article.objects.get(pk=1)
-    return render(request, 'blog/index.html', {'article': article})
+    articles = models.Article.objects.all()
+    return render(request, 'blog/index.html', {'articles': articles})
